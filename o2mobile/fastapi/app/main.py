fb1b696084fb25cd14317ff3d0066cf3bbb59d3c
O2xygeno2 <o2xygeno2@gmail.com>
2025-07-21 18:50:39 +0530

    Update main.py

--- o2mobile/fastapi/app/main.py
+++ o2mobile/fastapi/app/main.py
@@ -7,8 +7,18 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 @app.on_event("startup")
 async def startup():
+    return {"message": "Start Up"}
+
+
+@app.get("/db")
+def connect_database():
     # Create db tables
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    return {"DB Says": "Hello Shivank"}
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
 
 app.include_router(api_router, prefix="/api/v1")
