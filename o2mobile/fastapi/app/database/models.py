94241c78a46c2990da92f0abf56bb6908d935b48
O2xygeno2 <o2xygeno2@gmail.com>
2025-07-21 20:18:30 +0530

    Update models.py

--- o2mobile/fastapi/app/database/models.py
+++ o2mobile/fastapi/app/database/models.py
@@ -3,10 +3,17 @@ from sqlalchemy.dialects.postgresql import JSONB
 from geoalchemy2 import Geometry
 from app.database.connection import Base
 
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+
 class Location(Base):
     __tablename__ = "locations"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    # PostGIS geometry column (Point, 4326 = WGS84)
     geom = Column(Geometry(geometry_type='POINT', srid=4326))
+    properties = Column(JSONB)
