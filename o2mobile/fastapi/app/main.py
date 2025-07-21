5383c6eebf69d28b2bb68d6da4784ac44ece71bb
O2xygeno2 <o2xygeno2@gmail.com>
2025-07-21 14:50:30 +0530

    Create main.py

---
+++ o2mobile/fastapi/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.core.config import settings
+from app.database.connection import engine, Base
+from app.api.v1.routers import api_router
+
+app = FastAPI(title=settings.o2mobile)
+
+@app.on_event("startup")
+async def startup():
+    # Create db tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+app.include_router(api_router, prefix="/api/v1")
